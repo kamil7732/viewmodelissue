e38ef9daa646fb1186f1f2ac9182cc906d7d4cc9
kamil <kamil.kluziak@gmail.com>
2016-02-11 23:05:21 +0100

    .

--- client/pendrive_product.js
+++ client/pendrive_product.js
@@ -1,52 +1,13 @@
 Template.productPendriveFlashdriveIndex.onCreated(function () {
     var self = this;
     self.autorun(function () {
-        var page = parseInt(FlowRouter.getQueryParam("page"));
-        if(page < 0 || page === NaN){
-            page = 0;
-        }
-        var limit = 10;
-        var offset = page*limit;
-        self.subscribe('product-flashdrive-with-skip', offset, limit);
-        var productsCursor = Products.find({}).fetch();
-        var productsIds = productsCursor.map(function (p) {
-            return p._id
-        });
+
+        self.subscribe('product');
+
 
     });
 });
 
-Template.productPendriveFlashdriveIndex.rendered = function () {
-
-    // Add slimscroll to element
-    $('.full-height-scroll').slimscroll({
-        height: '100%'
-    })
-
-};
-
-
-Template.pendriveFlashdriveListLoop.viewmodel({
-    share: 'sharedIdShare',
-    editMode: false,
-    changeEditMode: function (edit) {
-        if (edit === this.editMode()) return;
-        if (!edit) this.sharedId(null);
-        this.editMode(edit);
-    },
-    openNewProduct: function () {
-        this.changeEditMode(false);
-        this.sharedId(null);
-        this.openProductModal();
-    },
-    openModal: function () {
-        $("#createSupplierModal").modal("show");
-    },
-    openButton: function () {
-        this.changeEditMode(false);
-        this.openModal();
-    }
-});
 
 Template.pendriveFlashdriveListLoop.helpers({
     products: function () {
@@ -54,47 +15,23 @@ Template.pendriveFlashdriveListLoop.helpers({
         return objects;
     }
 });
+
+Template.pendriveFlashdriveListLoop.viewmodel({
+    openButton: function(){
+        var object = new Product();
+        object.set({
+            name : "aaaaa"
+        });
+        object.save();
+        console.log('clicked');
+    }
+
+});
+
+//COMMENT ME OUT TO GET RID OFF PROBLEM
+//-------------------------------------
 Template.pendriveFlashdriveListLoopSingle.viewmodel({
 
-    edit: function () {
-        this.parent().sharedId(this._id());
-        this.parent().changeEditMode(true);
-        this.parent().openModal();
-    },
-    delete: function () {
-        this.parent().changeEditMode(false);
-        var object = Products.findOne({_id: this._id()});
-        swal({
-                title: "Czy na pewno chcesz?",
-                text: "Masz zamiar skasować pendrive!",
-                type: "warning",
-                showCancelButton: true,
-                confirmButtonColor: "#DD6B55",
-                confirmButtonText: "Taaa chcę (głupi pomysł)",
-                closeOnConfirm: false
-            },
-            function () {
-                object.remove();
-                swal("Skasowane!", "Mam nadzieję, że duma Cię rozpiera :P", "success");
-            });
-    },
-    editPhotos: function () {
-        this.parent().sharedId(this._id());
-        this.parent().openPhotoModal();
-    }
-
 
 });
-
-Template.pendriveFlashdriveListLoopSingle.helpers({
-    parent: function () {
-        var parentCategory = Categories.findOne({_id: this.categoryId});
-
-        if (parentCategory !== undefined) {
-            return parentCategory.name;
-        }
-        else {
-            return '';
-        }
-    }
-});
+//-------------------------------------
--- server/publications/Product.js
+++ server/publications/Product.js
@@ -1,30 +1,3 @@
-Meteor.publish('product-by-id', function (id) {
-    return Products.find({_id: id});
-});
-//-------------------------------------------------------
-Meteor.publish('product-all-ids-fields1', function (ids) {
-    return Products.find({_id: {$in: ids}},{fields: {name:1}});
-});
-
-Meteor.publish('product-flashdrive', function () {
-    return Products.find({_type: 'product-flashdrive'},{sort: {name:1}});
-});
-
-Meteor.publish('product-flashdrive-id-name-brand', function () {
-    return Products.find({_type: 'product-flashdrive'},{sort: {name:1}, fields: {_id: 1, name: 1,brand:1, _type: 1}});
-});
-
-Meteor.publish('product-flashdrive-by-id', function (id) {
-    return Products.find({_type: 'product-flashdrive', _id: id});
-});
-
-Meteor.publish('product-flashdrive-with-skip', function (skip, limit) {
-    Counts.publish(this, 'total_product_pendrive_flashdrive', Products.find({_type: 'product-flashdrive'}));
-    if(skip < 0){
-        skip = 0;
-    }
-    if(limit > 10){
-        limit = 10;
-    }
-    return Products.find({_type: 'product-flashdrive'},{sort: {name:1}, limit: limit, skip: skip});
+Meteor.publish('product', function () {
+    return Products.find({});
 });
